ed758b2616c5de66a72fd37184a574f32dcfd559
Yram Tetteh-Abotsi <99413491+Asheryram@users.noreply.github.com>
2025-11-06 15:55:34 +0000

    Custom reusable Dialog created

---
+++ src/components/custom/customDialog.tsx
@@ -0,0 +1,65 @@
+import { XIcon } from "@phosphor-icons/react";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "../ui/dialog";
+import { ReactNode } from "react";
+import { ButtonType2, ButtonType3 } from "./button";
+
+interface CustomDialogProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  title?: ReactNode;
+  children: ReactNode;
+  onSave?: () => void;
+  onCancel?: () => void;
+  isLoading?: boolean;
+  disabled?: boolean;
+  saveText?: string;
+  cancelText?: string;
+}
+
+export default function CustomDialog({
+  open,
+  onOpenChange,
+  title = "Title",
+  children,
+  onSave,
+  onCancel,
+  isLoading = false,
+  disabled = false,
+  saveText = "Save",
+  cancelText = "Cancel",
+}: CustomDialogProps) {
+  const handleCancel = () => {
+    onCancel?.();
+    onOpenChange(false);
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent className="p-0">
+        <DialogHeader className="rounded-md p-6 pb-4 border-b border-border-subtle sticky top-0 bg-background z-10 flex">
+          <DialogTitle className="text-xl font-semibold flex justify-between w-full">
+            {title}
+            <XIcon
+              className="cursor-pointer h-5 w-5"
+              onClick={() => onOpenChange(false)}
+            />
+          </DialogTitle>
+        </DialogHeader>
+        
+        {children}
+
+        <DialogFooter>
+          <div className="flex justify-end gap-3 px-6 py-4 border-t bg-background sticky bottom-0">
+            <ButtonType3 onClick={handleCancel} disabled={isLoading}>
+              {cancelText}
+            </ButtonType3>
+            <ButtonType2 onClick={onSave} disabled={disabled || isLoading}>
+              {isLoading ? 'Saving...' : saveText}
+            </ButtonType2>
+          </div>
+
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
