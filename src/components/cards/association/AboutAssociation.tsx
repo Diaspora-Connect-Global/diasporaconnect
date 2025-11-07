ffb81ddb5877ef171881755963dfff9ba28a9543
Yram Tetteh-Abotsi <99413491+Asheryram@users.noreply.github.com>
2025-11-07 22:45:12 +0000

    Fixed middle layout

--- src/components/cards/association/AboutAssociation.tsx
+++ src/components/cards/association/AboutAssociation.tsx
@@ -14,7 +14,7 @@ export default function AboutAssociation({ members, createdDate, visibility, des
   const t = useTranslations('static');
   return (
     <div className="flex justify-center items-center my-4">
-      <div className="min-w-72 min-h-74 bg-surface-default rounded-lg shadow-sm p-4 flex flex-col gap-3">
+      <div className="max-w-72 min-h-74 bg-surface-default rounded-lg shadow-sm p-4 flex flex-col gap-3">
         {/* Header Section */}
           <h2 className="text-lg font-semibold text-text-primary">{t("about")}</h2>
         <div className="flex items-center gap-2 text-text-secondary mb-1">
