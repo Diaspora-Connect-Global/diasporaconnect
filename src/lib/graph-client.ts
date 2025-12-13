e776a551fed2bff735597c3c85a6ba1106798c72
Yram Tetteh-Abotsi <99413491+Asheryram@users.noreply.github.com>
2025-12-13 15:20:50 +0000

    Breaking change: updated the headers

--- src/lib/graph-client.ts
+++ src/lib/graph-client.ts
@@ -1,18 +1,37 @@
+import { ApolloClient, HttpLink, InMemoryCache, ApolloLink } from "@apollo/client";
+import { SetContextLink } from '@apollo/client/link/context';
+import { authStorage } from '@/store/CentralPersist'; // Adjust path as needed
 
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client"
 const cache = new InMemoryCache();
+
 const httpLink = new HttpLink({
   uri: "https://api.diasporaconnectglobal.com/graphql", 
 });
-const gqlClient = new ApolloClient({
-    cache: cache,
-    link: httpLink,
-    queryDeduplication: false,
-    defaultOptions: {
-        watchQuery: {
-            fetchPolicy: "cache-and-network",
-        },
+
+
+
+const authLink = new SetContextLink((prevContext, operation) => {
+  const token = authStorage.getAccessToken();
+
+  return {
+    headers: {
+      ...prevContext.headers,
+      authorization: token ? `Bearer ${token}` : "",
     },
+  };
 });
 
-export default gqlClient
+
+// Combine the auth link and http link
+const gqlClient = new ApolloClient({
+  cache: cache,
+  link: authLink.concat(httpLink),
+  queryDeduplication: false,
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: "cache-and-network",
+    },
+  },
+});
+
+export default gqlClient;
