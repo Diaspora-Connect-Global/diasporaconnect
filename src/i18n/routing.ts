92caddcc9991a4c867922891b69cb8f00693359d
CoderLines <stephenbedz@gmail.com>
2026-04-24 14:20:47 +0000

    Refactor code structure for improved readability and maintainability

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,6 +1,6 @@
 import {defineRouting} from 'next-intl/routing';
  
 export const routing = defineRouting({
-  locales: ['en', 'fr', 'it',"de"],
+  locales: ['en', 'fr', 'it', 'de', 'nl'],
   defaultLocale: 'en'
 });
